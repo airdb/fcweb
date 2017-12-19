004bbeb05ccf3819f1878037895fc0a9e6e58158
Dean <dean@airdb.com>
2017-12-19 20:08:56 +0800

    update

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
+import Home from '@/components/home'
+import SignIn from '@/components/signin'
 import Main from '@/views/Main'
 
 Vue.use(Router)
@@ -10,11 +11,26 @@ export default new Router({
     {
       path: '/',
       name: 'Hello',
-      component: Hello
+      component: Home
     },
     {
       path: '/tree',
       name: 'Hello Ztree',
+      component: Main
+    },
+    {
+      path: '/home',
+      name: 'home',
+      component: Home
+    },
+    {
+      path: '/signin',
+      name: 'signin',
+      component: SignIn
+    },
+    {
+      path: '/signout',
+      name: 'signout',
       component: Main
     }
   ]
