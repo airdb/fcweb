4c82f383a2bbd65f3ca8f45f31fe221ebd7a41e3
Dean <dean@airdb.com>
2018-02-21 15:31:10 +0800

    modfiy

--- src/components/signin.vue
+++ src/components/signin.vue
@@ -2,6 +2,7 @@
   <div class="signin">
     <h1>登录</h1>
     <h1>{{ msg }}</h1>
+    <h1>{{ status }}</h1>
     <ul>
     <li><input type="text" v-model="username" ></li>
     <li><input type="password"  v-model="password"></li>
@@ -16,10 +17,12 @@
 export default {
   name: 'signin',
   data () {
-    this.apiurl = 'https://aws.airdb.io/lambda/Passport'
+    this.apiurl = 'https://airdb.io/lambda/Passport'
     return {
-      username: 'admin',
-      password: 'admin',
+      username: 'dean',
+      password: 'dean',
+      status: -1,
+      token: 'defualt',
       msg: 'Welcome to signin, https://aws.airdb.io/lambda/Passport'
     }
   },
@@ -29,15 +32,17 @@ export default {
       this.$http.get(this.apiurl).then((response) => {
         // 响应成功回调
         console.log(response.data)
-        this.username = response.status
-        this.password = response.token
-        this.message = response.body.message
+        console.log(response.data.status)
+        this.status = response.data.status
+        // this.username = response.status
+        // this.password = response.token
+        // this.message = response.body.message
       }, (response) => {
         // 响应错误回调
         console.log('fail!!!!')
-        this.result = 'fetch fail!!'
+        // this.result = 'fetch fail!!'
       })
-      alert(this.username, this)
+      // alert(this.username, this)
     }}
 }
 </script>
