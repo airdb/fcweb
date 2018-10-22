c838b3d82b19d3d1c1d28ed3b496d59754fe9246
99MyCql <949835972@qq.com>
2018-10-22 21:42:23 +0800

    modify bbs.vue by 99MyCql

--- src/components/bbs.vue
+++ src/components/bbs.vue
@@ -1,59 +1,125 @@
 <template>
-  <div class="hello">
-     <h1 @click="changeTitle">{{ msg }}</h1>  
-    <h1>{{ msg1 }}</h1>
-    <span v-bind:title="msg2" >
-    鼠标悬停几秒钟查看此处动态绑定的提示信息！
-    </span>
+<div>
+  <nav class="header navbar navbar-inverse navbar-static-top">
+    <div class="container">
+      <div class="navbar-header navbar-left" >
+        <a href="" class="navbar-brand">宝贝回家</a>
+      </div>
+      <!-- navbar-header end -->
+      
+      <div class="navbar-collapse collapse">
+        <ul class="nav navbar-nav">
+          <li class="nav-item" v-for="item in items">
+            <a class="nav-link" :href="item.url" target="_blank">{{item.name}}</a>
+          </li>
+        </ul>
+        <form class="navbar-form navbar-right">
+          <input class="form-control" type="text" placeholder="Search">
+          <button class="btn" type="button">Search</button>
+        </form>
+      </div>
+      <!-- navbar-collapse end -->
+    </div>
+  </nav>  
+  <!-- navbar end -->
 
-    <p v-if="seen">现在你看到我了</p>
+  <div class="main container">
+    <div class="page-header">
+      <h1>Hello World</h1>
+    </div>
 
-  <ol>
-    <li v-for="todo in todos">
-      {{ todo.text }}
-    </li>
-  </ol>
-  
-   <p>{{ message }}</p>
-  <button v-on:click="reverseMessage">逆转消息</button>
+    <!-- page-header end -->
+    <div class="row">
+      
+      <div class="col-md-9 col-sm-8">
+        <div class="row">
+          <h3>热门精华</h3>
+          <hr>
+          <div class="jumbotron">
+            <h1>Hello, world!</h1>
+            <p>This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
+            <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more &raquo;</a></p>
+          </div>
 
-  <p>{{ result }}</p>
+          <h3>云专栏</h3>
+          <hr>
+          <div class="jumbotron">
+            <h1>Hello, world!</h1>
+            <p>This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
+            <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more &raquo;</a></p>
+          </div>
 
+          <h3>友情链接</h3>
+          <hr>
+          <div class="jumbotron">
+            <h1>Hello, world!</h1>
+            <p>This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
+            <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more &raquo;</a></p>
+          </div>
+        </div>
+      </div>
+      <!-- col-md-9 end -->
+      <div class="col-md-3 col-sm-4">
+        <h3>hello</h3>
+        <div class="well">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sed diam eget risus varius blandit sit amet non magna. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Cras mattis consectetur purus sit amet fermentum. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Aenean lacinia bibendum nulla sed consectetur.
+          </p>
+        </div>
+        <h3>hello</h3>
+        <div class="well">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sed diam eget risus varius blandit sit amet non magna. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Cras mattis consectetur purus sit amet fermentum. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Aenean lacinia bibendum nulla sed consectetur.
+          </p>
+        </div>
+        <h3>hello</h3>
+        <div class="well">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sed diam eget risus varius blandit sit amet non magna. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Cras mattis consectetur purus sit amet fermentum. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Aenean lacinia bibendum nulla sed consectetur.
+          </p>
+        </div>
+      </div>
+      <!-- col-md-3 end -->
+    </div>
+    <!-- row end -->
   </div>
+  <!-- main end -->
+  
+  <div class="footer">
+    <div class="inner">
+      <h1>Hello, world!</h1>
+      <p>This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
+    </div>
+  </div>
+</div>
 </template>
 
 <script>
 export default {
-  name: 'hello',
-
   data () {
     return {
-      msg: 'msg: Welcome to Your Vue.js App',
-      msg1: 'msg1: Welcome to Your Vue.js App',
-      msg2: 'You loaded this page on ' + new Date().toLocaleString(),
-      seen: true,
-      message: 'Hello Vue.js!',
-
-      result: '',
-
-      todos: [
-        { text: '学习 JavaScript' },
-        { text: '学习 Vue' },
-        { text: '整个牛项目' }
+      items: [
+        {name: '宝贝回家', url: 'http://yq.aliyun.com/'},
+        {name: '宝贝回家', url: 'http://yq.aliyun.com/'},
+        {name: '宝贝回家', url: 'http://yq.aliyun.com/'},
+        {name: '宝贝回家', url: 'http://yq.aliyun.com/'},
+        {name: '宝贝回家', url: 'http://yq.aliyun.com/'},
+        {name: '宝贝回家', url: 'http://yq.aliyun.com/'},
+        {name: '宝贝回家', url: 'http://yq.aliyun.com/'}
       ]
     }
   },
   methods: {
-    changeTitle () {
-      this.msg = 'Wonderful Vue.js App'
-    },
-    reverseMessage () {
-      this.message = this.message.split('').reverse().join('')
-    }
-
   },
   created () {
-    this.fetchData()
   }
 }
 </script>
+
+<style>
+.footer {
+  padding: 40px;
+  background: #373d41;
+  color: #9b9ea0;
+}
+</style>
