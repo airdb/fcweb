83d087d372774a1969d289d75c786a6e16cd1c4e
99MyCql <949835972@qq.com>
2018-11-05 20:37:24 +0800

    modify article.vue

--- src/views/forum/article.vue
+++ src/views/forum/article.vue
@@ -27,8 +27,10 @@
         <router-link to="/forum/edit" class="btn btn-primary btn-md px-5" style="margin-left:450px">发表主题</router-link>
         <router-link to="/forum/edit" class="btn btn-primary btn-md px-5 mr-2">回复主题</router-link>
       </div>
+      <!-- head-button end -->
 
-      <div class="row bg-light">
+
+      <div class="row bg-light border-bottom">
         <div class="col-1 py-2 border-right text-center text-secondary" style="font-size:15px">
           浏览
           {{article.browseNum}}
@@ -46,50 +48,75 @@
         </div>
         <!-- col-10 end -->
       </div>
-      <!-- row end -->
+      <!-- title-row end -->
 
-      <div class="row mt-1 border-bottom border-secondary" v-for="item, index in items" :key="index">
-        <div class="col-2 bg-light">
-          <img src="../../assets/img2.jpg" class="mt-3 border" width="160px" height="160px">
-          <div class="text-center text-primary mt-2" style="font-size:20px">
-            {{article.author}}
+
+      <div v-for="item, index in items" :key="index">
+        <div class="row">
+          <div class="col-2 bg-light">
+            <img src="../../assets/img2.jpg" class="mt-3 border" width="160px" height="160px">
+            <div class="text-center text-primary mt-2" style="font-size:20px">
+              {{article.author}}
+            </div>
+            <div class="text-left text-secondary mt-2 ml-3" style="font-size: 12px">
+              发帖数<span class="ml-5">{{article.postNum}}</span>
+              <br>
+              粉丝数<span class="ml-5">{{article.fansNum}}</span>
+            </div>
           </div>
-          <div class="text-left text-secondary mt-2 ml-3" style="font-size: 12px">
-            发帖数<span class="ml-5">{{article.postNum}}</span>
-            <br>
-            粉丝数<span class="ml-5">{{article.fansNum}}</span>
+          <!-- col-2 end -->
+          <div class="col-10">
+            <div class="text-secondary py-2 border-bottom mb-1">
+              <span class="text-danger" v-if="index === 0">楼主  </span>发表于：{{article.date}}
+              <span class="float-right">{{index+1}} 楼</span>
+            </div>
+            <!-- header end -->
+
+            <div class="text-center">
+              <img src="../../assets/img4.jpeg" width="300px" height="300px">
+            </div>
+            <!-- content-img end -->
+            <div class="mt-3">
+              {{article.content}}
+            </div>
+            <!-- content-text end -->
+
+            <div class="my-5 text-center">
+              <router-link to="" class="btn btn-outline-primary btn-sm mr-2">收藏</router-link>
+              <router-link to="" class="btn btn-outline-primary btn-sm">关注</router-link>
+            </div>
+            <!-- footer end -->
           </div>
+          <!-- col-10 end -->
         </div>
-        <!-- col-2 end -->
+        <!-- articles-row end -->
+        <div class="row" style="height:5px">
+          <div class="col-2" style="background-color:#888888"></div>
+          <div class="col-10" style="background-color:#BBBBBB"></div>
+        </div>
+        <!-- articles-footer-row end -->
+      </div>
+      <!-- for-article end -->
+
+
+      <div class="row my-3">
         <div class="col-10">
-          <div class="text-secondary py-2 border-bottom mb-1">
-            <span class="text-danger" v-if="index === 0">楼主  </span>发表于：{{article.date}}
-            <span class="float-right">{{index+1}} 楼</span>
-          </div>
-          <!-- header end -->
-
-          <div class="text-center">
-            <img src="../../assets/img4.jpeg" width="300px" height="300px">
-          </div>
-          <!-- content-img end -->
-          <div class="mt-3">
-            {{article.content}}
-          </div>
-          <!-- content-text end -->
-
-          <div class="my-5 text-center">
-            <router-link to="" class="btn btn-outline-primary btn-sm mr-2">收藏</router-link>
-            <router-link to="" class="btn btn-outline-primary btn-sm">关注</router-link>
-          </div>
-          <!-- footer end -->
+          <nav class="">
+            <ul class="pagination">
+              <li class="page-item"><a class="page-link" href="#">Previous</a></li>
+              <li class="page-item"><a class="page-link" href="#">1</a></li>
+              <li class="page-item"><a class="page-link" href="#">2</a></li>
+              <li class="page-item"><a class="page-link" href="#">3</a></li>
+              <li class="page-item"><a class="page-link" href="#">Next</a></li>
+            </ul>
+          </nav>
         </div>
-        <!-- col-10 end -->
+        <div class="col-2">
+          <router-link to="/forum/edit" class="btn btn-primary btn-md px-5">发帖</router-link>
+        </div>
       </div>
-      <!-- row end -->
+      <!-- button-row end -->
 
-      <div class="row my-2">
-        <div class="btn btn-outline-secondary btn-md" style="width:100%">下一页>></div>
-      </div>
 
     </div>
     <!-- container end -->
