9c89b02116d4410e07c0228609275ee67995ae09
deancn <dean@airdb.com>
2021-06-26 02:11:51 +0800

    x

--- src/demo/tree.vue
+++ src/demo/tree.vue
@@ -1,40 +1,40 @@
 <template>
-    <div class="treeWrapper">
-        <NvTree
-            :items="items"
-            :search="search"
-            :checkbox="checkbox"
-            @on-check-change="onCheckChangeHandler"
-        ></NvTree>
-    </div>
+  <div class="treeWrapper">
+    <NvTree
+      :items="items"
+      :search="search"
+      :checkbox="checkbox"
+      @on-check-change="onCheckChangeHandler"
+    ></NvTree>
+  </div>
 </template>
 
 <script>
-import {getTree} from '../common/api';
+import { getTree } from "../common/api";
 
 export default {
-    name: 'nvTreeCheckedDemo',
-    data() {
-        const self = this;
-        return {
-            search: true,
-            checkbox: true,
-            items: {}
-        };
+  name: "nvTreeCheckedDemo",
+  data() {
+    const self = this;
+    return {
+      search: true,
+      checkbox: true,
+      items: {},
+    };
+  },
+  methods: {
+    onCheckChangeHandler: function () {
+      this.$Message.info("check changed");
     },
-    methods: {
-        onCheckChangeHandler: function () {
-            this.$Message.info('check changed');
-        }
-    },
-    mounted() {
-        var _this = this;
+  },
+  mounted() {
+    var _this = this;
 
-        getTree(this).then(function (resp) {
-            console.log(resp.data.data);
-            _this.items = resp.data;
-        });
-    }
+    getTree(this).then(function (resp) {
+      console.log(resp.data.data);
+      _this.items = resp.data.data;
+    });
+  },
 };
 </script>
 
